36e3a1263cc83b3cac269118fa7230145f8f57a6
YasaMufasa <43527639+YasaMufasa@users.noreply.github.com>
2020-09-05 15:59:13 -0600

    Adding the system architecture and building the initial web scraper.

---
+++ Source/sma.py
@@ -0,0 +1,55 @@
+'''
+Stock Market Analysis
+by Josh Gardner
+
+The purpose of this project is to complete a basic analysis of the stock market while gathering the data via web
+scraping. The gathered data is from Yahoo Finance, https://finance.yahoo.com.
+
+The first thing to do when web scraping is to check Yahoo's robots.txt file. Yahoo Finance has the following in their
+robots.txt file:
+
+    User-agent: *
+    Sitemap: https://finance.yahoo.com/sitemap_en-us_desktop_index.xml
+    Sitemap: https://finance.yahoo.com/sitemaps/finance-sitemap_index_US_en-US.xml.gz
+    Sitemap: https://finance.yahoo.com/sitemaps/finance-sitemap_googlenewsindex_US_en-US.xml.gz
+    Disallow: /m/
+    Disallow: /r/
+    Disallow: /__rapidworker-1.2.js
+    Disallow: /__blank
+    Disallow: /_td_api
+    Disallow: /_remote
+
+This states that for all users, any website extension that does not contain any of the Disallowed web extensions can be
+scraped without any issue. This is good news for this project. This project will focus on scrapping data from the three
+following websites:
+
+    1. https://finance.yahoo.com/most-active
+    2. https://finance.yahoo.com/gainers
+    3. https://finance.yahoo.com/losers
+
+To be polite, the initial code developed for this project will not be automated and will only scrap Yahoo Finance when
+the code is run. When the code is developed further to include automation, the timing of the web scraping will be set to
+not cause any undue stress on Yahoo's servers.
+
+Now that we have checked the robots.txt file for what is allowed, we can focus on building our web scraper. We will be
+using BeautifulSoup for our web scraper. The functions we will be using are saved in the sma_funcs.py file.
+'''
+
+from sma_funcs import *
+
+
+# These are the three pages of the most active stocks. This will grab up to 300 stocks.
+most_active_urls = ['https://finance.yahoo.com/most-active?count=100&offset=0',
+       'https://finance.yahoo.com/most-active?count=100&offset=100',
+       'https://finance.yahoo.com/most-active?count=100&offset=200']
+
+# These are the three pages of the gainers.
+gainers_urls = ['https://finance.yahoo.com/gainers?offset=0&count=100',
+                'https://finance.yahoo.com/gainers?count=100&offset=100',
+                'https://finance.yahoo.com/gainers?count=200&offset=100']
+
+# These are the three pages of the losers.
+losers_urls = ['https://finance.yahoo.com/losers?offset=0&count=100',
+               'https://finance.yahoo.com/losers?offset=100&count=100',
+               'https://finance.yahoo.com/losers?offset=200&count=100']
+
---
+++ Source/sma_funcs.py
@@ -0,0 +1,39 @@
+'''
+Stock Market Analysis - Functions
+by Josh Gardner
+
+This file contains the functions to be used in the Stock Market Analysis project.
+'''
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+# This function scrapes the stock data from the list of urls and combines them into a single data frame.
+def get_data(url_list=[],Cols=['symbol', 'name', 'price_(intraday)', 'change', 'percent_change', 'volumn', '3_month_avg_vol', 'market_cap', 'pe_ratio']):
+
+    # Creates an empty dataframe to store the scraped data
+    dataframes = []
+
+    # scrapes across the urls in the url_list to gather the data using BeautifulSoup
+    for url in url_list:
+        page = requests.get(url)
+        bs = BeautifulSoup(page.content, 'html.parser')
+        # We're only interest in the data in the table. We search for the data listed in the table
+        tb = bs.find('table')
+        # We're only interested in the data labeled with the 'tr' tag in the table.
+        rows = tb.find_all('tr')
+        cell_data = []
+        for row in rows:
+            cells = row.find_all('td')
+            cells = cells[0:9]
+            cell_data.append([cell.text for cell in cells])
+
+        # Here's where we add the data into a DataFrame. However, each individual url results in a different DataFrame.
+        dataframes.append(pd.DataFrame(cell_data, columns=Cols).drop(0, axis=0))
+
+    # This is where the different DataFrames are merged together into a single DataFrame.
+    final_data = pd.concat(dataframes)
+    # This returns the final DataFrame with all data in a single DataFrame
+    return final_data
